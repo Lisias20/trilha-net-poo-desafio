febf36090c010ca64b1309681d14903b4b6cf47d
Lisias20 <matrikigamerlol@gmail.com>
2024-01-11 17:49:45 -0300

    Desafio concluido

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+    
+        public Iphone(string numero, string Modelo, string IMEI, int Memoria) : base(numero, Modelo, IMEI, Memoria)
+        {
+
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+             Console.WriteLine($"Instalando {nomeApp} na App Store do iPhone...");
+
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia(numero: "81-981105148", Modelo: "Nokia G11plus", IMEI: "IMEI98723234", Memoria: 256);
+Smartphone iphone = new Iphone(numero: "81-982048253", Modelo: "Iphone 15 PRO max", IMEI: "IMEI982736173", Memoria: 580);
+
+
+
+Console.WriteLine("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬");
+Console.WriteLine("Celular: Nokia");
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+Console.WriteLine("\n");
+Console.WriteLine("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬");
+
+
+Console.WriteLine("Celular: Iphone");
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+iphone.InstalarAplicativo("Instagram");
+Console.WriteLine("\n");
+Console.WriteLine("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬");
+
+
